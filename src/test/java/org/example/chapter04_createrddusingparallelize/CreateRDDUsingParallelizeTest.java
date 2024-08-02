d1b72651b0ddc0bb6885541b3dd2dfa630d07001
davidsan <david.sanluis@qrsof.com>
2024-08-01 21:28:10 -0600

    add: - test java collection Streams - test with partitions rdd

--- src/test/java/org/example/chapter04_createrddusingparallelize/CreateRDDUsingParallelizeTest.java
+++ src/test/java/org/example/chapter04_createrddusingparallelize/CreateRDDUsingParallelizeTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CreateRDDUsingParallelizeTest {
 
@@ -34,4 +36,56 @@ public class CreateRDDUsingParallelizeTest {
             System.out.printf("Number of partitions: %d%n", defaultRDD.getNumPartitions() );
         }
     }
+
+    @Test
+    @DisplayName("Create a Spark RDD from Java Collection using parallelize method")
+    void createSparkRDDUsingParallelizeMethod(){
+
+        try(final var sparkContext = new JavaSparkContext(sparkConf)) {
+            List<Integer> data = Stream.iterate(1, n -> n + 1)
+                    .limit(8L)
+                    .collect(Collectors.toList());
+            JavaRDD<Integer> myRDD = sparkContext.parallelize(data);
+            System.out.println(myRDD);
+            System.out.printf("Number of partitions: %d%n", myRDD.getNumPartitions() );
+            System.out.printf("Total elements in RDD: %d%n", myRDD.count() );
+            System.out.println("Elements of RDD:" );
+            myRDD.collect().forEach(System.out::println);
+
+            // reduce operations
+            Integer max = myRDD.reduce(Integer::max);
+            Integer min = myRDD.reduce(Integer::min);
+            Integer sum = myRDD.reduce(Integer::sum);
+
+            System.out.printf("Max~>%d:, Min~>%d, Sum~>%d", max, min, sum);
+
+        }
+    }
+
+    @Test
+    @DisplayName("Create a Spark RDD from Java Collection using parallelize method with given partitions")
+    void createSparkRDDUsingParallelizeMethodWithGivenPartitions(){
+
+        try(final var sparkContext = new JavaSparkContext(sparkConf)) {
+            List<Integer> data = Stream.iterate(1, n -> n + 1)
+                    .limit(8L)
+                    .collect(Collectors.toList());
+            JavaRDD<Integer> myRDD = sparkContext.parallelize(data, 8);
+            System.out.println(myRDD);
+            System.out.printf("Number of partitions: %d%n", myRDD.getNumPartitions() );
+            System.out.printf("Total elements in RDD: %d%n", myRDD.count() );
+            System.out.println("Elements of RDD:" );
+            myRDD.collect().forEach(System.out::println);
+
+            // reduce operations
+            Integer max = myRDD.reduce(Integer::max);
+            Integer min = myRDD.reduce(Integer::min);
+            Integer sum = myRDD.reduce(Integer::sum);
+
+            System.out.printf("Max~>%d:, Min~>%d, Sum~>%d", max, min, sum);
+
+        }
+    }
+
+
 }

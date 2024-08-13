06e9cb87d3a895fedfc3e12d2349b28087503b7b
davidsan <david.sanluis@qrsof.com>
2024-08-12 22:48:30 -0600

    add: - Test loading local text files into Spark RDD Using Method Source

--- src/test/java/org/example/chapter05_rddexternaldatasets/RDDExternalDatasetsTest.java
+++ src/test/java/org/example/chapter05_rddexternaldatasets/RDDExternalDatasetsTest.java
@@ -6,7 +6,12 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class RDDExternalDatasetsTest {
 
@@ -30,4 +35,27 @@ public class RDDExternalDatasetsTest {
             System.out.println("-------------\n");
         }
     }
+
+    @ParameterizedTest
+    @MethodSource("getFilePaths")
+    @DisplayName("Test loading local text files into Spark RDD Using Method Source")
+    void testLoadingLocalTextFileIntoSparkRDDUsingMethodSource(final String localFilePath) {
+
+        try (final var sparkContext = new JavaSparkContext(sparkConf)){
+            JavaRDD<String> myRdd = sparkContext.textFile(localFilePath);
+
+            System.out.printf("Total lines in the file: %d%n", myRdd.count());
+            System.out.println("Printing first 10 lines->");
+            myRdd.take(10).forEach(System.out::println);
+            System.out.println("-------------\n");
+        }
+    }
+
+    private static Stream<Arguments>getFilePaths(){
+        return Stream.of(
+                Arguments.of(Path.of("src","test","resources","1000words.txt").toString()),
+                Arguments.of(Path.of("src","test","resources","wordslist.txt.gz").toString())
+        );
+    }
+
 }
